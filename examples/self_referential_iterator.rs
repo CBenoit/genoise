4db84f51f14eaaa6e811169a4219918f44e4c62c
Benoît CORTIER <bcortier@proton.me>
2023-10-07 21:10:08 -0400

    docs: more examples

--- examples/self_referential_iterator.rs
+++ examples/self_referential_iterator.rs
@@ -1,44 +1,4 @@
-use genoise::local;
-use genoise::{Co, GeneratorFlavor};
-
-// Such an iterator would be hard to write by hand in today’s stable Rust
-async fn deep_iterator_impl<'a, T, F>(mut co: Co<'_, &'a T, (), F>, slice_of_slices: &[&'a [T]])
-where
-    F: GeneratorFlavor,
-{
-    if slice_of_slices.is_empty() {
-        return;
-    }
-
-    for slice in slice_of_slices {
-        for value in *slice {
-            co.suspend(value).await;
-        }
-    }
-}
-
-// Thin wrapper around a generator
-pub struct DeepIterator<'a, T> {
-    // Implementation detail
-    inner: local::Gn<'a, 'a, &'a T, (), ()>,
-}
-
-impl<'a, T> DeepIterator<'a, T> {
-    pub fn new(slice_of_slices: &'a [&'a [T]]) -> Self {
-        let inner = local::Gn::new(|co| deep_iterator_impl(co, slice_of_slices));
-        Self { inner }
-    }
-}
-
-impl<'a, T> IntoIterator for DeepIterator<'a, T> {
-    type Item = &'a T;
-
-    type IntoIter = local::Gn<'a, 'a, &'a T, (), ()>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.inner
-    }
-}
+use library::DeepIterator;
 
 fn main() {
     let it = DeepIterator::new(&[&[1, 2, 3], &[4, 5, 6], &[7, 8, 9]]);
@@ -47,5 +7,49 @@ fn main() {
     for (expected, actual) in expected.iter().zip(it) {
         assert_eq!(actual, expected);
         println!("{actual}");
+    }
+}
+
+mod library {
+    use genoise::local;
+    use genoise::{Co, GeneratorFlavor};
+
+    // Such an iterator would be hard to write by hand in today’s stable Rust
+    async fn deep_iterator_impl<'a, T, F>(mut co: Co<'_, &'a T, (), F>, slice_of_slices: &[&'a [T]])
+    where
+        F: GeneratorFlavor,
+    {
+        if slice_of_slices.is_empty() {
+            return;
+        }
+
+        for slice in slice_of_slices {
+            for value in *slice {
+                co.suspend(value).await;
+            }
+        }
+    }
+
+    // Thin wrapper around a generator
+    pub struct DeepIterator<'a, T> {
+        // Implementation detail not exposed to the user
+        inner: local::Gn<'a, 'a, &'a T, (), ()>,
+    }
+
+    impl<'a, T> DeepIterator<'a, T> {
+        pub fn new(slice_of_slices: &'a [&'a [T]]) -> Self {
+            let inner = local::Gn::new(|co| deep_iterator_impl(co, slice_of_slices));
+            Self { inner }
+        }
+    }
+
+    impl<'a, T> IntoIterator for DeepIterator<'a, T> {
+        type Item = &'a T;
+
+        type IntoIter = local::Gn<'a, 'a, &'a T, (), ()>;
+
+        fn into_iter(self) -> Self::IntoIter {
+            self.inner
+        }
     }
 }
